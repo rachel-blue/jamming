3b759fa2686efe91aebf41c382bb9f390dd7e38d
Rachel Blue <rachel17.22@gmail.com>
2020-03-08 12:22:41 -0600

    Updates

---
+++ precache-manifest.83d39537e1946cd8a379aefc283170b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcdc4926ffbb288debcbc296d8f6e5c",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "c0cbacc597479bb347b6",
+    "url": "/jamming/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "779ce64fcf594f8529c6",
+    "url": "/jamming/static/js/2.161a84be.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/jamming/static/js/2.161a84be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cbacc597479bb347b6",
+    "url": "/jamming/static/js/main.744885b3.chunk.js"
+  },
+  {
+    "revision": "6ebc3dbdb8bf740a34ae",
+    "url": "/jamming/static/js/runtime-main.78172c84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jamming/static/media/logo.5d5d9eef.svg"
+  }
+]);
