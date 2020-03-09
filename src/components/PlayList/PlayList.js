2dd8f1876b7c5b38ab9ebaa744eea58674fe399d
Rachel Blue <rachel17.22@gmail.com>
2020-03-09 12:03:24 -0600

    static props passed for playlist

--- src/components/PlayList/PlayList.js
+++ src/components/PlayList/PlayList.js
@@ -2,14 +2,18 @@ import React from 'react';
 import TrackList from '../TrackList/TrackList';
 import './PlayList.css';
 
-function PlayList() {
-  return (
-    <div className="Playlist">
-      <input defaultValue={'New Playlist'}/>
-      <TrackList tracks={[]} />
-      <button className="Playlist-save">SAVE TO SPOTIFY</button>
-    </div>
-  );
+class PlayList extends React.Component {
+  render() {
+    return (
+      <div className="Playlist">
+        <input defaultValue={'New Playlist'}/>
+        <TrackList
+          tracks={this.props.playlistTracks}
+        />
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    );
+  }
 }
 
 export default PlayList;
